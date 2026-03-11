de301d55d60186296c35677e35d2620181822d2e
Christian Evangelista <chriismatos@icloud.com>
2026-03-11 02:09:20 -0300

    config api

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://evfleet-frontend-rgke.vercel.app'],
     credentials: true,
   });
 
