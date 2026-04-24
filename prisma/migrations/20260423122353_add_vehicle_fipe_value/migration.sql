867438d4d97b7af6c431c227f0c316a63ec5d51a
Christian Evangelista <chriismatos@icloud.com>
2026-04-23 22:39:53 -0300

    frontend: 0.8.4

--- prisma/migrations/20260423122353_add_vehicle_fipe_value/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Subscription" DROP CONSTRAINT "Subscription_companyId_fkey";
-
--- AlterTable
-ALTER TABLE "Vehicle" ADD COLUMN     "fipeValue" DECIMAL(12,2);
-
--- AddForeignKey
-ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
